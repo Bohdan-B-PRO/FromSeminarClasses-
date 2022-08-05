92a2afb66a155e24821221eb47dc37bc1565c288
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-05 20:21:55 +0300

    Added info Lessen 1

---
+++ Lessen_1/Program.cs
@@ -0,0 +1,32 @@
+﻿//LESSEN 1
+//Task 1
+/*
+int num1, num2;
+
+Console.Write ("input first integer nimber " );
+num1=Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write ("input second integer nimber " );
+num2=Convert.ToInt32(Console.ReadLine()); 
+ 
+ if(num1==num2*num2)
+ {
+    Console.WriteLine("Yes");
+ }
+ else
+ {
+Console.WriteLine("No");
+ }
+
+//Tsask 2
+Console.Write ("input posittive integer nimber " );
+int num=Convert.ToInt32(Console.ReadLine()); 
+
+int current= num * (-1);
+
+while(current <= num)
+   {
+   Console.Write(current + " ");
+   current++;
+   } 
+   */
