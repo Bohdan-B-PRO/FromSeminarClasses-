ee39ecb81743ac3ff98f12f03aa1caf0854d33cf
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-05 20:34:33 +0300

    Added info Lessen 3

---
+++ Lessen_3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Lessen 3
+
+/*
+void ShowDiapason(int quart)
+{
+if (quart >=1 && quart <=4 )
+{
+if(quart==1) Console.WriteLine("x>0, y>0");
+if(quart==1) Console.WriteLine("x<0, y>0");
+if(quart==1) Console.WriteLine("x<0, y<0");
+if(quart==1) Console.WriteLine("x>0, y<0");
+}
+else Console.WriteLine("Uncorrect input ");
+}
+Console.WriteLine("Input number of oquart");
+int quartNum=Convert.ToInt32(Console.ReadLine());
+ShowDiapason(quartNum);
+*/
+
+
