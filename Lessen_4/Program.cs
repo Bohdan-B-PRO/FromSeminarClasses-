147f9d28d7dbafb926c3e3cc990a91c7142c2f3f
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-05 20:43:14 +0300

    Added info Lessen 4

---
+++ Lessen_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Lessen 4
+/*
+int NumOfDitges(int num)
+{
+    int count = 0;
+    while (num != 0)//num > 0
+    {
+        num /= 10;//тоже самое что nam = nam / 10! 
+        count++;
+    }
+    return count;
+}
+Console.Write("Input intenger nimber ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numOfDigits = NumOfDitges(number);
+Console.WriteLine($"Number of ditgest of {number} is {numOfDigits} ");
+*/
